0a2bc2fb5fe2b376fb64b29f093f6c10cec0e6ce
Arnar Nesset <arnarnesset@hotmail.com>
2017-09-29 16:05:24 +0200

    Ny installasjon pga. krøll med db-connection.

--- doh/Properties/AssemblyInfo.cs
+++ doh/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("doh")]
+[assembly: AssemblyTitle("DOH")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("doh")]
+[assembly: AssemblyProduct("DOH")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ef2f572d-855a-44d4-a7f5-0a579a9f1ce4")]
+[assembly: Guid("c51ba47a-788c-45bb-a10a-c91a5eaed34a")]
 
 // Version information for an assembly consists of the following four values:
 //
